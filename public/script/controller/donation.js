44101ff5531c8debd0a491bf952900ff07b7fbc7
AlejoNext <alejonext@Lambda.local>
2016-06-10 19:42:07 -0500

    Ok changes in donate

--- public/script/controller/donation.js
+++ public/script/controller/donation.js
@@ -1,8 +1,11 @@
 export default class done {
-	static $inject = ['$scope', '$rootScope'];
+	static $inject = ['$scope', '$rootScope', '$anchorScroll'];
 	static n = 'done';
 	
-	constructor(scope, doc) {
+	constructor(scope, doc, scroll) {
 		doc.maxBar = true;
-	}	
+		scope.$watch(() => this.isbay, newval => angular.isDefined(newval) ? scroll('step2') : null )
+		scope.$watch(() => this.isType, newval => angular.isDefined(newval) ? scroll('step3') : null )
+	}
+
 }
